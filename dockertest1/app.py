3a68150de90f2177c9f5fec9c6c63bcee0549519
onesun8191 <onesun8191@gmail.com>
2023-02-27 09:43:18 +0900

    app.run(host='0.0.0.0', port='5001')

--- dockertest1/app.py
+++ dockertest1/app.py
@@ -4,11 +4,13 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'flask.render_template("index.html")'
+    # return flask.render_template("index.html")
+    return 'please press the like btn!!!'
 
 @app.route('/home')
 def aaa():
     return flask.render_template("aaa.html", a=70, b=30)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port='5001')

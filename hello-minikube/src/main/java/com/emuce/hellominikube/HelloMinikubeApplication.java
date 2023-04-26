bf5b660bd3e84f2b63e98afbb93e5fa2a2fd6c7f
onesun8191 <onesun8191@gmail.com>
2023-04-26 14:09:52 +0900

    initial

---
+++ hello-minikube/src/main/java/com/emuce/hellominikube/HelloMinikubeApplication.java
@@ -0,0 +1,13 @@
+package com.emuce.hellominikube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMinikubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMinikubeApplication.class, args);
+	}
+
+}
